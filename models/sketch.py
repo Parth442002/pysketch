0967b2c3e406412d89010a12b9d0dd3d58d58167
Parth442002 <parthkatiyar04@gmail.com>
2022-04-27 14:13:23 +0530

    Added Sketch.py

--- models/sketch.py
+++ models/sketch.py
@@ -1,15 +1,39 @@
 import cv2
+from django.forms import NullBooleanField
 import matplotlib.pyplot as plt
+from zmq import NULL
 
 
-def sketch(img_path):
-    img = cv2.imread(img_path)
-    RGB_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+def img2sketch(photo, hd=NULL):
+    if hd == NULL:
+        k_size = 111
+    elif hd == 'High Definition':
+        k_size = 333
+    elif hd == 'Standard Definition':
+        k_size = 55
+    # Read Image
+    img = cv2.imread(photo)
+
+    # Convert to Grey Image
     grey_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # Invert Image
     invert_img = cv2.bitwise_not(grey_img)
-    blur_img = cv2.GaussianBlur(invert_img, (111, 111), 0)
+    # invert_img=255-grey_img
+
+    # Blur image
+    blur_img = cv2.GaussianBlur(invert_img, (k_size, k_size), 0)
+
+    # Invert Blurred Image
     invblur_img = cv2.bitwise_not(blur_img)
+    # invblur_img=255-blur_img
+
+    # Sketch Image
     sketch_img = cv2.divide(grey_img, invblur_img, scale=256.0)
-    # saving the Image
-    cv2.imwrite('./sketches/sketch.png', sketch_img)
-    return plt.imshow(sketch_img)
+
+    # Save Sketch
+    #cv2.imwrite('sketch.png', sketch_img)
+    rgb_sketch_img = cv2.cvtColor(sketch_img, cv2.COLOR_BGR2RGB)
+
+    # Display sketch
+    plt.imshow(rgb_sketch_img)
